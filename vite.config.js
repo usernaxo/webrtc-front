341c1c31f688f4c6c6dee03a1eee3c4dfb6894d7
usernaxo <ignacioantoniogomezrodriguez@gmail.com>
2025-10-14 20:30:44 -0300

    webrtc-front

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import fs from 'fs'
-import path from 'path'
+// import fs from 'fs'
+// import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '.cert/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, '.cert/cert.pem')),
-    },
-    host: '0.0.0.0',
-    port: 5174
-  }
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync(path.resolve(__dirname, '.cert/key.pem')),
+  //     cert: fs.readFileSync(path.resolve(__dirname, '.cert/cert.pem')),
+  //   },
+  //   host: '0.0.0.0',
+  //   port: 5174
+  // }
 })
